d2095e2203f2501149f90337a1ae38cac629ae01
Sam Moser <smoser7@gmail.com>
2015-04-16 16:27:18 -0500

    rearranged code so its more readable

--- Vision/regionvocr.py
+++ Vision/regionvocr.py
@@ -9,28 +9,36 @@ TXT_EXTENSION = ".txt"
 
 possibleChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','!','@','#','$','%','^','&','*','(',')']
 
-capture = cv2.VideoCapture(0)
 
-#take out these two lines for cam on robot (needed for my webcam at home)
-capture.set(3, 160)
-capture.set(4, 120)
+def initCamera():
+    #add these two lines for cam on robot (otherwise opencv times out when captureing image)
+    os.system("rmmod uvcvideo")
+    os.system("modprobe uvcvideo nodrop=1 timeout=5000")
 
-#add these two lines for cam on robot (otherwise opencv times out when captureing image)
-os.system("rmmod uvcvideo")
-os.system("modprobe uvcvideo nodrop=1 timeout=5000")
+    capture = cv2.VideoCapture(0)
 
-#display images (comment out when running on beaglebone)
-cv2.namedWindow('capture', 0)
+    #take out these two lines for cam on robot (needed for my webcam at home)
+    capture.set(3, 160)
+    capture.set(4, 120)
 
-while True:
+    #display images (comment out when running on beaglebone)
+    cv2.namedWindow('capture', 0)
 
+    return capture
+
+def captureFrame(capture):
     ret,img = capture.read()
+    if(ret == False):
+        print "capture failed"
+        return ret
 
     #comment out imshow when running on beaglebone 
     cv2.imshow('capture', img)
-
     cv2.imwrite(IMAGE_FILE, img) 
 
+    return ret
+
+def getCharacterFromImageFile():
     #characters file must be place in tesseract config folder ( /tesseract/share/tessdata/config/)
     shret = commands.getoutput("tesseract " + IMAGE_FILE + " " + TXT_FILE + " -psm 10" + " characters")
 
@@ -38,10 +46,17 @@ while True:
     character = data.read()
     #check if string is empty
     if not character:
-        continue
+        return None
     #check if its one of our possibleChars
     if character[0] in possibleChars:
-        print character[0]
+        return character[0]
+    else:
+        return None     #use if is None: to check if return is null
 
-    sleep(2)
+def main():
+    cap = initCamera()
+    while True:
+        ret = captureFrame(cap)
+        sleep(2)
 
+main()
