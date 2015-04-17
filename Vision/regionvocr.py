889c2af873991039591d8f7816ee592200d79fea
Sam Moser <smoser7@gmail.com>
2015-04-16 22:53:44 -0500

    regionvocr working on beaglebone

--- Vision/regionvocr.py
+++ Vision/regionvocr.py
@@ -3,7 +3,7 @@ import os
 import commands
 from time import sleep
 
-IMAGE_FILE = "temp.png"
+IMAGE_FILE = "temp.jpg"
 TXT_FILE = "data"
 TXT_EXTENSION = ".txt"
 
@@ -11,52 +11,66 @@ possibleChars = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'
 
 
 def initCamera():
-    #add these two lines for cam on robot (otherwise opencv times out when captureing image)
-    os.system("rmmod uvcvideo")
-    os.system("modprobe uvcvideo nodrop=1 timeout=5000")
+	#add these two lines for cam on robot (otherwise opencv times out when captureing image)
+	os.system("rmmod uvcvideo")
+	os.system("modprobe uvcvideo nodrop=1 timeout=5000")
 
-    capture = cv2.VideoCapture(0)
+	capture = cv2.VideoCapture(0)
 
-    #take out these two lines for cam on robot (needed for my webcam at home)
-    capture.set(3, 160)
-    capture.set(4, 120)
+	#take out these two lines for cam on robot (needed for my webcam at home)
+	#capture.set(3, 160)
+	#capture.set(4, 120)
+	
+	#display images (comment out when running on beaglebone)
+	#cv2.namedWindow('capture', 0)
 
-    #display images (comment out when running on beaglebone)
-    cv2.namedWindow('capture', 0)
-
-    return capture
+	return capture
 
 def captureFrame(capture):
-    ret,img = capture.read()
-    if(ret == False):
-        print "capture failed"
-        return ret
+	ret,img = capture.read()
+	if(ret == False):
+		print "capture failed"
+	else:
+		print "image capture"
 
-    #comment out imshow when running on beaglebone 
-    cv2.imshow('capture', img)
-    cv2.imwrite(IMAGE_FILE, img) 
-
-    return ret
+	#comment out imshow when running on beaglebone 
+	#cv2.imshow('capture', img)
+	print "writing img"
+	cv2.imwrite(IMAGE_FILE, img) 
+	capture.release()
 
 def getCharacterFromImageFile():
-    #characters file must be place in tesseract config folder ( /tesseract/share/tessdata/config/)
-    shret = commands.getoutput("tesseract " + IMAGE_FILE + " " + TXT_FILE + " -psm 10" + " characters")
+	#characters file must be place in tesseract config folder ( /tesseract/share/tessdata/config/)
+	shret = commands.getoutput("tesseract " + IMAGE_FILE + " " + TXT_FILE + " -psm 10" + " characters")
 
-    data = open(TXT_FILE + TXT_EXTENSION)
-    character = data.read()
-    #check if string is empty
-    if not character:
-        return None
-    #check if its one of our possibleChars
-    if character[0] in possibleChars:
-        return character[0]
-    else:
-        return None     #use if is None: to check if return is null
+	data = open(TXT_FILE + TXT_EXTENSION)
+	character = data.read()
+	data.close()
+	#check if string is empty
+	if not character:
+		print "string from data.txt is empty"
+		return None
+	#check if its one of our possibleChars
+	if character[0] in possibleChars:
+		return character[0]
+	else:
+		print "character not on possible character list"
+		print "character is: " + character[0]
+		return None     #use if is None: to check if return is null
 
 def main():
-    cap = initCamera()
-    while True:
-        ret = captureFrame(cap)
-        sleep(2)
+	while True:
+		cap = initCamera()
+		captureFrame(cap)
+		char = getCharacterFromImageFile()
+		if char is not None:
+			print "found: "
+			print char
+		else:
+			print "nothing found"
+
+		k = raw_input('hit enter to continue')
+		if k == 'q':
+			break
 
 main()
